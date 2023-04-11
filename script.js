4c9788a82d86c1d3c2c798c002fc1f31af1b2fe6
unknown <otmaneechchafyky5@gmail.com>
2023-04-11 08:45:03 +0000

    create and Update script.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuContainer = document.getElementById('menu_container');
+const hamburger = document.getElementById('menu_icon');
+const xMenu = document.getElementById('x-menu');
+const menuLink = document.querySelectorAll('.menu-link');
+
+hamburger.addEventListener('click',() => {
+    menuContainer.style.display = 'block';
+});
+
+xMenu.addEventListener('click',() => {
+    menuContainer.style.display = 'none';
+});
+
+for(let i = 0; i < menuLink.length; i++){
+    menuLink[i].addEventListener('click',() => {
+        menuContainer.style.display = 'none';
+    });
+}
