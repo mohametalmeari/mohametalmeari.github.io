dcb7566d41f26ac3b2abff680586d946d8843b9d
mohametalmeari <mohamet.almeari@gmail.com>
2023-04-12 21:24:07 +0300

    Store Form Data

--- script.js
+++ script.js
@@ -186,3 +186,31 @@ form.addEventListener('submit', (event) => {
     form.submit();
   }
 });
+
+// Form Input Local storage
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      (e.code === 22 ||
+        e.code === 1014 ||
+        e.name === "QuotaExceededError" ||
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+function populateStorage() {
+  localStorage.setItem('name', document.getElementById('name').value);
+  localStorage.setItem('email', document.getElementById('email').value);
+  localStorage.setItem('message', document.getElementById('message').value);
+}
