ef0e6e7f5e154322e8429039dd365c18f4d012b1
mohametalmeari <mohamet.almeari@gmail.com>
2024-01-28 21:31:15 +0300

    Improve SEO & Refactor bio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,28 @@
 import { Link } from 'react-router-dom';
 import {
   GithubIcon,
-  LinkedinIcon, MediumIcon, TwitterIcon,
+  LinkedinIcon,
+  MediumIcon,
+  TwitterIcon,
 } from '../assets/icons';
 import Particle from './Particle';
 import TypeHeader from './TypeHeader';
+import { bio } from '../redux/data';
 
 const Header = () => (
   <>
     <header id="home">
       <Particle />
-      <h1>
-        <TypeHeader />
-      </h1>
-      <p className="header-text">
-        I&#39;m a software developer! I can help you build a product, feature or website.
-        Look through some of my work and experience!
-        If you like what you see and have a project you need coded,
-        don&#39;t hesitate to contact me.
-      </p>
+      <h1 style={{ zIndex: '-999', position: 'absolute', top: '-100%' }}>Mohamad - Web Developer</h1>
+      <h2 className="type-header"><TypeHeader /></h2>
+      <p className="header-text">{bio}</p>
       <span>
         <button
           className="connect-link"
           type="button"
-          onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })}
+          onClick={() => document
+            .getElementById('contact')
+            .scrollIntoView({ behavior: 'smooth' })}
         >
           LET&#39;S CONNECT
         </button>
